574626ae4a8fada895d947de0882882d8f7ae522
Ken Yamada <yamakenth@gmail.com>
2021-12-22 23:02:51 +0700

    Create function to get forecast data

--- src/weather-api.js
+++ src/weather-api.js
@@ -16,12 +16,17 @@ async function getCurrentWeather(city) {
 }
 
 // get forecast weather data 
-// take in current weather obj
+// take in coordinate from current weather obj
 // return data 
-async function getForecast(obj) {
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${obj.lat}&lon=${obj.lon}&exclude=minutely,hourly,alerts&appid=${API_KEY}`;
-  
-  console.log(url);
+async function getForecast(coord) {
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&exclude=minutely,hourly,alerts&appid=${API_KEY}`;
+  try {
+    const response = await fetch(url, { mode: 'cors' });
+    const data = await response.json();
+    return data;
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 // get weather from city name input 
@@ -29,7 +34,7 @@ async function getForecast(obj) {
 // return data
 async function getWeather(cityName) {
   const currentWeather = await getCurrentWeather(cityName);
-  const forecast = await getForecast(currentWeather);
+  const forecast = await getForecast(currentWeather.coord);
 
   return forecast;
 }
