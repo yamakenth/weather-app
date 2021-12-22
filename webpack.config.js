3fec68feab4ddd12d32220d62cb7cfdd3da98beb
Ken Yamada <yamakenth@gmail.com>
2021-12-22 19:14:41 +0700

    Setup webpack development environment

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,13 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
